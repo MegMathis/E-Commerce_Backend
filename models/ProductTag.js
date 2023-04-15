c551d45d1bacec50e35a64d709b79970a25abf38
MegMathis <mcdonough.megan25@gmail.com>
2023-04-15 18:05:47 -0400

    models_completed

---
+++ models/ProductTag.js
@@ -0,0 +1,42 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+
+// import our database connection from config.js
+const sequelize = require("../config/connection");
+
+// Initialize ProductTag (table) by extending off Sequelize's Model class
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Tag",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product_tag",
+  }
+);
+
+module.exports = ProductTag;
